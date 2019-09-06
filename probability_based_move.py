c31dbb1d611e0487433ff1db6d4f9562cf92cbbe
jiangdonghan <2641635339@qq.com>
2019-09-06 16:50:52 +1000

    All two functions finished

--- probability_based_move.py
+++ probability_based_move.py
@@ -15,7 +15,8 @@ from AIMA.logic import *
 from AIMA.utils import *
 from AIMA.probability import *
 from tkinter import messagebox
-
+    
+import logic_based_move as logic_move
 #--------------------------------------------------------------------------------------------------------------
 #
 #  The following two functions are to be developed by you. They are functions in class Robot. If you need,
@@ -43,50 +44,53 @@ from tkinter import messagebox
 # This function will be called in the constructor of class Robot in the main program the_wumpus_world.py to construct the
 # joint probability distribution object. Your function next_room_prob() will need to use the joint probability distribution
 # to calculate the required conditional probabilities.
+
+
+
 #
+#   ** 03/09/2019 Developed by XU Han
+#   ** Parameters: set, list, tuple, or dict
+#   ** Description: 
+#   ** 1. The function is to change types, such as list, tuple and set, to a list type.
+#   ** 2. The function is to deal with each item in the transformtion of data to String type.  
+#   ** 3. The funciton is to truncate each item with String type. (Space Removal)
+#   ** Return: list
+#   ** Example: List -> List / [(1,  2), (3,3)] => ['(1,2)', '(3,3)']
+#   ** Example: Set -> List / {(1,  2), (3,3)} => ['(1,2)', '(3,3)']
+#
+def data_standardised(v_list):
+    v_list = list(v_list)
+    for i in range(len(v_list)):
+        v_list[i] = str(v_list[i]).replace(' ','')
+        v_list[i] = ''+v_list[i]+''
+    return v_list
+    
+T, F = True, False 
 def PitWumpus_probability_distribution(self, width, height): 
     # Create a list of variable names to represent the rooms. 
     # A string '(i,j)' is used as a variable name to represent a room at (i, j)
-    
     self.PW_variables = [] 
     for column in range(1, width + 1):
         for row in range(1, height + 1):
             self.PW_variables  = self.PW_variables  + ['(%d,%d)'%(column,row)]
 
     #--------- Add your code here -------------------------------------------------------------------
-    self.PW_variables.remove('(1,1)')
-    knownpw = self.observation_pits(self.visited_rooms)
-    knownbs = self.observation_breeze_stench(self.visited_rooms)
-    available_rooms = list(self.available_rooms)
-    number_of_pit = self.cave.number_of_pit
-    p_false =0.2 #should be replaced by self.max_pit_probability  # w/p happen
-    p_true = 1-0.2#should be replaced by 1 - self.max_pit_probability # not happen
-    
-    var_values = {each: [T, F] for each in available_rooms}
-    Pr_N_rooms = JointProbDist(available_rooms, var_values)
-    events = all_events_jpd(available_rooms, Pr_N_rooms, {})
-
+    p_false = 0.8  # w/p happen
+    p_true = 1 - p_false # not happen
+    var_values = {each: [T, F] for each in self.PW_variables}
+    jdP_PWs = JointProbDist(self.PW_variables, var_values)
+    events = all_events_jpd(self.PW_variables, jdP_PWs, {})
     for each_event in events:
-    # Calculate the probability for this event
-    # if the value of a variable is false, motiply by p_false which is 0.2, otherwise motiply by p_true which is 1-0.2 
+        # Calculate the probability for this event
+        # if the value of a variable is false, motiply by p_false which is 0.12, otherwise motiply by p_true which is 1-0.12 
         prob = 1 # initial value of the probability
-        ##没有考虑 stentch和breeze
         for (var, val) in each_event.items(): # for each (variable, value) pair in the dictionary
-            if val == F:
-                prob = prob * p_false
-            else: prob = prob * p_true
-    # Assign the probability to this event
-        Pr_N_rooms[each_event]= prob
-        
-    return Pr_N_rooms
- 
-    
-    
-    
-            
-        
-#---------------------------------------------------------------------------------------------------
-#   Function 2. next_room_prob(self, x, y)
+            prob = prob * p_false if val == F else prob * p_true
+        # Assign the probability to this event
+        jdP_PWs[each_event]= prob
+    return jdP_PWs
+    #---------------------------------------------------------------------------------------------------
+#  Function 2. next_room_prob(self, x, y)
 #
 #  The parameters, (x, y), are the robot's current position in the cave environment.
 #  x: column
@@ -103,37 +107,71 @@ def PitWumpus_probability_distribution(self, width, height):
 #    3. If the probabilities of all the surrounding rooms are not lower than the pre-specified probability
 #       threshold, return (0,0).
 #
-def next_room_prob(self, x, y):
+def next_room_prob(self, x, y):   
+    self.next_move = (0,0)
+    self.next_move = logic_move.next_room(self, x, y)
     
-    knownpw = self.observation_pits(self.visited_rooms)
-    knownbs = self.observation_breeze_stench(self.visited_rooms)
-    rquery = self.cave.getsurrounding(x,y)
-    new_room = (0,0)
-    ##删选安全的room
-    for each_s in rquery:
-        if each_s not in self.visited_rooms:
-            # call check_safety() to do a resolution reasoning to find a safe room
-            if (self.check_safety(each_s[0],each_s[1]) == False):  
-                rquery.remove(each_s) 
-    lowest_pr = 1
-    for each_room in rquery:
-        ##选出最小的概率，没有则返回（0，0）a
-        pr_query = enumerate_joint_ask(each_room,{},self.PitWumpus_probability_distribution(self.cave.WIDTH,self.cave.HEIGHT)) 
+    if(self.next_move != (0,0)):
+        return self.next_move
+    else:
+        # When the room is not safe, it requires to consider case 2.
+        # Create a Set to store all visited rooms
+        r_know = data_standardised(self.visited_rooms)
         
-        if pr_query.prob[False] < lowest_pr:
-            lowest_pr = pr_query.prob[False]
-            new_room = each_room
+        # Create a list to rooms that is available to move
+        r_query = data_standardised(self.cave.getsurrounding(x, y))
+        for room in r_know:
+            if room in r_query:
+                r_query.remove(room)
             
-    if lowest_pr > 0.2:  #should be replaced by self.max_pit_probability
-            return (0,0)    
-    return new_room        
-    
-    #--------- Add your code here -------------------------------------------------------------------
+        # Create a list to rooms that is to store unknown area.
+        r_other = data_standardised(self.PW_variables)
+        for room in r_know:
+            if room in r_other:
+                r_other.remove(room)
+            
+        for room in r_query:
+            if room in r_other:
+                r_other.remove(room)
+        
+        # Create a dict to store the room with the probability of PITS & WUMPUS
+        known_pw = self.observation_pits(self.visited_rooms)
+        # Create a dict to store the room with the probability of Breeze & Stench
+        known_bs = self.observation_breeze_stench(self.visited_rooms)
+        lowest_pr = 1
+        for Pq in r_query:
+            # the variable is used for the caculation of probabilities in True
+            sum_true = 0 
+            # the variable is used for the caculation of probabilities in False
+            sum_false = 0 
+            # the variable is used for the caculation of probabilities in T / T + F
+            prob = 0
+            
+            # R_unknown = R_other + (R_query - Pq)
+            r_unknown = r_other + r_query
+            r_unknown.remove(Pq)
+            print(self.jdP_PWs.show_approx())
+            # Initialisation of the PW
+            known_pw.update({Pq : T})
+            events_true = all_events_jpd(r_unknown, self.jdP_PWs, known_pw)
+            for event in events_true: 
+                sum_true += self.consistent(known_bs, event) * self.jdP_PWs[event]
 
-
-
-
-
-#---------------------------------------------------------------------------------------------------
- 
+            known_pw[Pq] = F
+            events_false = all_events_jpd(r_unknown, self.jdP_PWs, known_pw)
+            for event in events_false: 
+                sum_false += self.consistent(known_bs, event) * self.jdP_PWs[event] 
+        
+            # Nomalisation of the Probability
+            prob = sum_true / (sum_true + sum_false)
+            r_unknown.append(Pq)
+            # Getting the lowest probability in each loop
+            if (lowest_pr > prob):
+                lowest_pr = prob
+                self.next_move = tuple(eval(Pq))
+     
+        if(lowest_pr > self.max_pit_probability):
+            return (0,0)
+        else:
+            return self.next_move
 ####################################################################################################
--- temp.py
+++ temp.py
@@ -5,15 +5,13 @@ Spyder Editor
 This is a temporary script file.
 """
 temp = []
-width = 4
-height = 4
-number_pits = 3
+width = 2
+height = 2
 for column in range(1, width + 1):
         for row in range(1, height + 1):
             temp  = temp  + ['(%d,%d)'%(column,row)]
-temp.remove('(1,1)')
-p_false = 0.2  # w/p happen
-p_true = 1 - 0.2 # not happen
+p_false = 0.8  # w/p happen
+p_true = 1 - 0.8 # not happen
 var_values = {each: [T, F] for each in temp}
 Pr_N_rooms = JointProbDist(temp, var_values)
 # Pr_N_rooms_initial = pow(p_false,number_pits+1)*pow(p_true,width*height-number_pits-2)
@@ -28,14 +26,28 @@ for each_event in events:
     # Assign the probability to this event
     Pr_N_rooms[each_event]= prob
 
-# print(Pr_N_rooms.show_approx())
 
-for each in temp:
-    p = enumerate_joint_ask(each, {}, Pr_N_rooms)
-    print(each,  p.show_approx())
-count = 0       
+#print(Pr_N_rooms.show_approx())
+
+ 
 # for event in self.visted_rooms:
-    
-visited_rooms = set()
-visited_rooms.add((1,2))
-type(visited_rooms)
+room = '(1,1)'
+#temp.remove('(1,1)')
+print(temp)
+temp = temp[1:]
+#temp = temp[1:]
+#print(var_values)
+print(temp)
+events_true = all_events_jpd(temp, Pr_N_rooms, {room : True})
+sum_true = 0
+for event in events_true: 
+    sum_true+=(Pr_N_rooms[event]*
+    print(event)
+    print(Pr_N_rooms[event])
+
+
+# =============================================================================
+# for each in temp:
+#     p = enumerate_joint_ask(each, {}, Pr_N_rooms)
+#     print(each,  p.show_approx())
+# =============================================================================
